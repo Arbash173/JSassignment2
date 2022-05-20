9895d8e0d204063317efd61ebc4f660d3455fe4c
Arbash173 <arbashhumayun0@gmail.com>
2022-05-20 08:45:13 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,172 @@
+// chapter 3
+
+// Q01
+// var age = prompt("Enter Your age")
+// alert("You are " + age +" years old")
+
+// Q2
+// var visited ="You have visited the site"
+// alert(visited + " Nth time")
+
+// Q3
+// var birthYear = 1998
+// document.write("Your Birth Year is " + birthYear)
+
+// Q4
+// var name = prompt("Enter Your Name")
+// var product = prompt("Enter your Product","shirt, jeans, shorts, huddies")
+// var quantity = prompt("Enter Quantity","etc 5,3,7")
+// document.write(name + " Ordered " + quantity + product +"(s) "+" on xyz clothing store")
+
+// chapter 4
+
+// Q1
+// var name,age,nic;
+
+// Q2
+// var name,age,$arbash,fatherName,mother_name
+// var let,123,7name,#name,hyphen@
+
+// Q3
+// document.write("A heading stating Rules for naming JS variables ")
+// document.write("<br>");
+// document.write("Variable names can only contain number, $,and _.")
+// document.write("<br>");
+// document.write("Variable must begin with a letter,$ or _")
+// document.write("<br>");
+// document.write("variable names are case sensitive")
+// document.write("<br>");
+// document.write("variable names should not be JS keywords")
+
+// Chapter 5
+
+// Q1
+// var num1 = 3;
+// var num2 = 5;
+// result = num1 + num2;
+// document.write("The sum of 3 and 5 is " + result)
+// document.write("<br>");
+
+// Q2
+// var num1 = 3;
+// var num2 = 5;
+// result = num1 - num2;
+// document.write("The subtraction of 3 and 5 is " + result)
+// document.write("<br>");
+// var num1 = 3;
+// var num2 = 5;
+// result = num1 * num2;
+// document.write("The product of 3 and 5 is " + result)
+// document.write("<br>");
+
+// var num1 = 3;
+// var num2 = 5;
+// result = num1 / num2;
+// document.write("The division of 3 and 5 is " + result)
+// document.write("<br>");
+
+// var num1 = 3;
+// var num2 = 5;
+// result = num1 % num2;
+// document.write("The remainder of 3 and 5 is " + result)
+
+// Q3
+// var num
+// document.write("value after variable declaration is " +num)
+// document.write("<br>");
+// num = 5;
+// document.write("Initial value " + num)
+// num++;
+// document.write("<br>");
+// document.write("Value after increment "+num)
+// num = num + 7;
+// document.write("<br>");
+// document.write("value after addition is" +num)
+// num--;
+// document.write("<br>");
+// document.write("Value after decrement "+num)
+// num = num % 3;
+// document.write("<br>");
+// document.write("The Remainder is "+num)
+
+// Q4
+// var ticketCost = 600;
+// var nofticket = prompt("Enter the number of tickets You want","1,2,4,10...")
+// var totalprice = ticketCost * nofticket;
+// document.write("Total cost to buy " + nofticket +" Tickets to a movie is " +totalprice)
+
+// Q5
+//  var num = parseInt(prompt('Enter an integer: '));
+    
+// document.write("the table of "+num)
+// for(let i = 1; i <= 10; i++) {
+
+   
+//     const result = i * num;
+//     document.write("<br>");
+
+
+//     // display the result
+    
+//     document.write(num +"*" +i +"=" +result )
+// }
+
+// Q6
+// var celsius = prompt("Enter the temperture")
+// var farenheit;
+// farenheit = (celsius - 32)*5/9
+
+// document.write(celsius +" C "+" is "+farenheit +"F")
+
+// var farenheit = prompt("Enter the temperture")
+// var celsius;
+// celsius = (celsius * 9/5) + 32
+
+// document.write(farenheit +" F "+" is "+celsius +"C")
+
+// Q7
+// document.write("Shopping Cart")
+// document.write("<br>");
+// var price01 = 650;
+// var price02 = 100;
+// var quantityItem1 = prompt("Enter Number of item1")
+// var quantityItem2 = prompt("Enter Number of item2")
+// var totalCost = (price01 * quantityItem1) + (price02 * quantityItem2) + 100;
+
+// document.write("Price of item 1 is 650Rs")
+// document.write("<br>");
+// document.write("Quantity of item 1 is " +quantityItem1)
+// document.write("<br>");
+// document.write("Price of item 2 is 100Rs")
+// document.write("<br>");
+// document.write("Quantity of item 2 is " +quantityItem2)
+// document.write("<br>");
+// document.write("Shipping Charges 100Rs")
+// document.write("<br>");
+// document.write("Total cost of your order is "+totalCost + "Rs")
+
+// Q8
+// obtainMarks = prompt("enter marks you obtain")
+// totalMarks = 980;
+// percentage = obtainMarks/totalMarks * 100;
+
+// document.write("Total Marks: 980")
+// document.write("<br>");
+// document.write("Marks you Obtain " + obtainMarks)
+// document.write("<br>");
+// document.write("Percentage " + percentage +"%")
+
+// Q9
+// var totalDollar = ("Enter your amount in $")
+// totalPkr = totalDollar*200;
+// document.write("Total Currency in PKR " +totalPkr + "Rs")
+
+// var totalRiyal = ("Enter your amount in riyal")
+// totalPkr = totalRiyal*54;
+// document.write("Total Currency in PKR " +totalPkr + "Rs")
+
+// Q10
+
+
+
+
